0c8709df910a47e5fc92d433d3d6b058221d6887
pedro felippe <pedfelippe@gmail.com>
2021-03-19 20:05:10 -0300

    add: adicionando endpoints GET: Composicao, Compositor, Periodo Musical, Interprete

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,8 @@ def home():
 
 # PLAYLIST
 
-@app.route('/playlist')
-def getPlaylists(self):
+@app.route('/playlist') # ok
+def getPlaylists():
     cursor.execute("SELECT * from Playlist")
     playlists = []
     for row in cursor.fetchall():
@@ -20,8 +20,8 @@ def getPlaylists(self):
                         'tempo_duracao':row[4]})
     return jsonify(playlists)
 
-@app.route('/playlist/<int:id>')    
-def getPlaylist(self, id):
+@app.route('/playlist/<int:id>')  # ok  
+def getPlaylist(id):
     query = "SELECT * from Playlist where PLAYLIST_ID=?"
     row = cursor.execute(query, id).fetchone()
     if row:
@@ -33,7 +33,7 @@ def getPlaylist(self, id):
     return {'message': 'Playlist não encontrada'}, 404
 
 @app.route('/playlist', methods=['POST'])
-def createPlaylist(self, request):
+def createPlaylist(request):
     data = request.get_json()
     playlist = {'nome': data['nome'], 
                 'descricao': data['descricao']}
@@ -54,7 +54,7 @@ def deletePlaylist(self, id):
 # ALBUM
 
 # GET /album
-@app.route('/album')
+@app.route('/album') # ok
 def getAlbums():
     cursor.execute("SELECT * from Album")
     albums = []
@@ -70,7 +70,7 @@ def getAlbums():
     return jsonify(albums)
 
 # GET /album/<int:id>
-@app.route('/album/<int:id>')
+@app.route('/album/<int:id>') # ok
 def getAlbum(id):
     query = "SELECT * FROM Album WHERE ALBUM_ID=?"
     row = cursor.execute(query, id).fetchone()
@@ -80,7 +80,7 @@ def getAlbum(id):
                     'descricao':row[2], 
                     'data_gravacao':row[3], 
                     'data_compra':row[4], 
-                    'preco_compra':row[5], 
+                    'preco_compra':(float(row[5])), 
                     'tipo_compra':row[6], 
                     'cod_gravadora':row[7]}
     return {'message':'Álbum não encontrado'}, 404
@@ -88,7 +88,7 @@ def getAlbum(id):
 # GRAVADORA
 
 ## TODO: GET /gravadora
-@app.route('/gravadora')
+@app.route('/gravadora') # ok
 def getGravadoras():
     cursor.execute("SELECT * from Gravadora")
     gravadoras = []
@@ -102,7 +102,7 @@ def getGravadoras():
     return jsonify(gravadoras)
 
 # GET /gravadora/<int:id>
-@app.route('/gravadora/<int:id>')
+@app.route('/gravadora/<int:id>') # ok
 def getGravadora(id):
     query = "SELECT * FROM Gravadora WHERE GRAVADORA_ID=?"
     row = cursor.execute(query, id).fetchone()
@@ -112,17 +112,30 @@ def getGravadora(id):
                     'telefone':row[2], 
                     'site':row[3], 
                     'nome':row[4]}
-    return {'message':'Álbum não encontrado'}, 404
+    return {'message':'Gravadora não encontrado'}, 404
 
 # FAIXA
 
 # TODO: GET: /faixa
-@app.route('/track')
+@app.route('/track') # ok
 def getTracks():
-    return
+    cursor.execute("SELECT * from Faixas")
+    faixas = []
+    for row in cursor.fetchall():
+        faixas.append({'FAIXA_ID':row[0],
+                        'album_id': row[1], 
+                        'tipo_composicao':row[2], 
+                        'numero_faixa':row[3], 
+                        'nome':row[4],
+                        'tempo_musical':row[5],
+                        'descricao':row[6],
+                        'tipo_gravacao':row[7],
+                        'duracao':row[8]
+                        })
+    return jsonify(faixas)
 
 # GET /faixa/<int:id>
-@app.route('/track/<int:id>')
+@app.route('/track/<int:id>') # ok
 def getTrack(id):
     query = "SELECT * FROM Faixas WHERE FAIXA_ID=?"
     row = cursor.execute(query, id).fetchone()
@@ -144,48 +157,120 @@ def getTrack(id):
 # TODO: GET: /interprete
 @app.route('/interprete')
 def getInterpretes():
-    return
+    cursor.execute("SELECT * from Interprete")
+    interpretes = []
+    for row in cursor.fetchall():
+        interpretes.append({'INTERPRETE_ID':row[0],
+                        'nome': row[1], 
+                        'tipo':row[2]
+                        })
+    return jsonify(interpretes)
 
 # TODO: GET /composicao/<int:id>
 @app.route('/interprete/<int:id>')
 def getInterprete(id):
-    return
+    query = "SELECT * FROM Interprete WHERE INTERPRETE_ID=?"
+    row = cursor.execute(query, id).fetchone()
+    if row:
+        return {'INTERPRETE_ID':row[0],
+                        'nome': row[1], 
+                        'tipo':row[2]
+                        }
+
+    return {'message':'Composição não encontrada'}, 404
 
 # COMPOSICAO
 
 # TODO: GET: /composicao
 @app.route('/composicao')
-def getComposicoes():
-    return
+def getComposicoes(): # ok
+    cursor.execute("SELECT * from Composicao")
+    composicoes = []
+    for row in cursor.fetchall():
+        composicoes.append({'COMPOSICAO_ID':row[0],
+                        'nome': row[1], 
+                        'descricao':row[2]
+                        })
+    return jsonify(composicoes)
 
 # TODO: GET /composicao/<int:id>
 @app.route('/composicao/<int:id>')
-def getComposicao(id):
-    return
+def getComposicao(id): # ok
+    query = "SELECT * FROM Composicao WHERE COMPOSICAO_ID=?"
+    row = cursor.execute(query, id).fetchone()
+    if row:
+        return {'COMPOSICAO_ID':row[0],
+                        'nome': row[1], 
+                        'descricao':row[2]
+                        }
+
+    return {'message':'Composição não encontrada'}, 404
 
 # PERIODO MUSICAL
 
 # TODO: GET: /periodo
 @app.route('/periodo')
 def getPeriodos():
-    return
+    cursor.execute("SELECT * from PeriodoMusical")
+    periodos = []
+    for row in cursor.fetchall():
+        periodos.append({'PERIODOMUSICAL_ID':row[0],
+                        'nome': row[1], 
+                        'descricao':row[2],
+                        'tempo_atividade': row[3]
+                        })
+    return jsonify(periodos)
 
 # TODO: GET /periodo/<int:id>
 @app.route('/periodo/<int:id>')
 def getPeriodo(id):
-    return
+    query = "SELECT * FROM PeriodoMusical WHERE PERIODOMUSICAL_ID=?"
+    row = cursor.execute(query, id).fetchone()
+    if row:
+        return {'PERIODOMUSICAL_ID':row[0],
+                        'nome': row[1], 
+                        'descricao':row[2],
+                        'tempo_atividade': row[3]
+                        }
+
+    return {'message':'Periodo Musical não encontrada'}, 404
 
 # COMPOSITOR
 
 # TODO: GET: /compositor
 @app.route('/compositor')
-def getCompositores():
-    return
+def getCompositores(): # nao quer funcionar?
+    cursor.execute("SELECT * from Compositor")
+    compositores = []
+    for row in cursor.fetchall():
+        compositores.append({'COMPOSITOR_ID':row[0],
+                'nome': row[1], 
+                'cidade_natal':row[2],
+                'pais_natal': row[3],
+                'data_nasc': row[4],
+                'data_morte': row[5],
+                'periodo_musical': row[6]
+                        })
+
+    return jsonify(compositores)
 
 # TODO: GET /compositor/<int:id>
 @app.route('/compositor/<int:id>')
 def getCompositor(id):
-    return
+    query = "SELECT * FROM Compositor WHERE COMPOSITOR_ID=?"
+    row = cursor.execute(query, id).fetchone()
+    if row:
+        return {
+                    'COMPOSITOR_ID':row[0],
+                    'nome': row[1], 
+                    'cidade_natal':row[2],
+                    'pais_natal': row[3],
+                    'data_nasc': row[4],
+                    'data_morte': row[5],
+                    'periodo_musical': row[6]
+                }
+
+    return {'message':'Composição não encontrada'}, 404
 
 
 # FAIXAS-COMPOSITOR-AUX
